13a8ae1cb6be6b70736556257519050c04881d67
allpha23 <allpha23@gmail.com>
2022-04-05 13:42:27 -0300

    Updates

---
+++ precache-manifest.09bfdd98f49b928e65308e40471682ff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "006a05477272ec0cb83d6c8f877b0a52",
+    "url": "/app-toot-name/index.html"
+  },
+  {
+    "revision": "359678f77fff8eb1d75f",
+    "url": "/app-toot-name/static/css/main.a7d2e540.chunk.css"
+  },
+  {
+    "revision": "a00083bdc82c7ae5cd13",
+    "url": "/app-toot-name/static/js/2.a2b84f7e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/app-toot-name/static/js/2.a2b84f7e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "359678f77fff8eb1d75f",
+    "url": "/app-toot-name/static/js/main.45b6f1c6.chunk.js"
+  },
+  {
+    "revision": "66ac581fbc8990623b1d",
+    "url": "/app-toot-name/static/js/runtime-main.cfc30c8d.js"
+  }
+]);
