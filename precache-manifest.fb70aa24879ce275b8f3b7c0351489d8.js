316077a326358c69123ab587b791c208acea0a24
allpha23 <allpha23@gmail.com>
2022-09-02 08:17:14 -0300

    Updates

---
+++ precache-manifest.fb70aa24879ce275b8f3b7c0351489d8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d8579cd51da3ad60d4242e1952d31b6",
+    "url": "/trybewallet/index.html"
+  },
+  {
+    "revision": "f918d063d92f51ec8d01",
+    "url": "/trybewallet/static/css/main.b16f8337.chunk.css"
+  },
+  {
+    "revision": "a00083bdc82c7ae5cd13",
+    "url": "/trybewallet/static/js/2.a2b84f7e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/trybewallet/static/js/2.a2b84f7e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f918d063d92f51ec8d01",
+    "url": "/trybewallet/static/js/main.dd2bf065.chunk.js"
+  },
+  {
+    "revision": "5dc72ebdcdec490a7442",
+    "url": "/trybewallet/static/js/runtime-main.ec83dc77.js"
+  }
+]);
